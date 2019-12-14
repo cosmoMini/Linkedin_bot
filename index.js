6104d24d65a0f98f4fab243d0f76dc6fde58ebd3
Rajeshwarii09 <rajiraghu1266@gmail.com>
2019-12-14 08:44:01 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+var express = require('express')
+  , passport = require('passport')
+  , LinkedinStrategy = require('../lib').Strategy;
+
+// API Access link for creating client ID and secret:
+// https://www.linkedin.com/secure/developer
+var LINKEDIN_CLIENT_ID = 81j1irsj8abe5e;
+var LINKEDIN_CLIENT_SECRET = HuegLtKR9lRACyyy;
+var CALLBACK_URL = process.env.CALLBACK_URL || 'http://localhost:3000/auth/linkedin/callback';
+
+
+// Passport session setup.
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the LinkedinStrategy within Passport.
+
+passport.use(new LinkedinStrategy({
+    clientID:     LINKEDIN_CLIENT_ID,
+    clientSecret: LINKEDIN_CLIENT_SECRET,
+    callbackURL:  CALLBACK_URL,
+    scope:        ['r_liteprofile', 'r_emailaddress','r_emailaddress'],
+    passReqToCallback: true
+  },
+  function(req, accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    req.session.accessToken = accessToken;
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+
+
+
+
+var app = express();
+
+// configure Express
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.logger());
+app.use(express.cookieParser());
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+// GET /auth/linkedin
+app.get('/auth/linkedin',
+  passport.authenticate('linkedin', { state: 'SOME STATE' }),
+  function(req, res){
+    // The request will be redirected to Linkedin for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/linkedin/callback
+app.get('/auth/linkedin/callback',
+  passport.authenticate('linkedin', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+var http = require('http');
+
+http.createServer(app).listen(3000);
+
+
+// Simple route middleware to ensure user is authenticated.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
